c0f9b1d4d023733adc5c1981b0332b7f9a7f2561
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-06-07 18:10:37 -0400

    Initial commit

---
+++ ShouldIDateCalculator/DateCalculator.h
@@ -0,0 +1,13 @@
+//
+//  DateCalculator.h
+//  ShouldIDateCalculator
+//
+//  Created by Ronald Hernandez on 6/7/15.
+//  Copyright (c) 2015 Wahoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DateCalculator : NSObject
+
+@end
---
+++ ShouldIDateCalculator/DateCalculator.m
@@ -0,0 +1,14 @@
+//
+//  DateCalculator.m
+//  ShouldIDateCalculator
+//
+//  Created by Ronald Hernandez on 6/7/15.
+//  Copyright (c) 2015 Wahoo. All rights reserved.
+//
+
+#import "DateCalculator.h"
+
+@implementation DateCalculator
+
+
+@end
